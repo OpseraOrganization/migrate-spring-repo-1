cd71eabb62f9db030a7c0141a2d71ce70cf613a2
Purusothaman Srinivasan <60213224+purusothamanns@users.noreply.github.com>
2022-12-20 17:31:18 +0530

    Update subnet.tf

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,16 +1,9 @@
 variable "platform_vpc_id" {
   type = string
 }
-variable "region" {
-  type = string
-}variable "access_key" {
-  type = string
-}variable "secret_key" {
-  type = string
-}
 
 provider "aws" {
-  region     = var.region
+  region     = "us-east-2"
   access_key = var.access_key
   secret_key = var.secret_key
 }
@@ -31,12 +24,6 @@ locals {
 locals {
   subnet_ids_list = [for i in range(length(local.availability_zone)): [for s in data.aws_subnet.example: s.id if s.availability_zone == local.availability_zone[i]][0]]  
 }
-
-# resource "aws_ec2_transit_gateway_vpc_attachment" "plaform-attachment" {
-#   subnet_ids         = local.subnet_ids_list
-#   transit_gateway_id = "tgw-0d8334d10d7ebf471"
-#   vpc_id             = "vpc-0ea4ddb307a38ed7b"
-# }
 
 output "subnet_ids_list" {
   value = local.subnet_ids_list
