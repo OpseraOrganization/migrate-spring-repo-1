13e150c4ea74a6066223b8254bc4e283d42f42d6
Purusothaman Srinivasan <60213224+purusothamanns@users.noreply.github.com>
2023-02-08 16:35:16 +0530

    Update HelloWorldController.java

--- src/main/java/com/opsera/controller/HelloWorldController.java
+++ src/main/java/com/opsera/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping
     public String hello() {
-        return "Opsera Testing with storage test";
+        return "Opsera Testing with storage rollback test";
     }
 }
