367eee8b060caa01a8c4855f4e2c799682b768b0
ramyaj726 <118713095+ramyaj726@users.noreply.github.com>
2023-04-03 19:26:36 +0530

    Update HelloWorldController.java

--- src/main/java/com/opsera/controller/HelloWorldController.java
+++ src/main/java/com/opsera/controller/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
     @GetMapping
     public String hello() {
-        return "Opsera Testing with nexus rollback test";
+        return "Opsera Testing with nexus rollback test1";
          
     }
 }
