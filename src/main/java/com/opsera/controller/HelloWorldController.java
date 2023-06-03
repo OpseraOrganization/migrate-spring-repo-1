7eb56cc96adbe56087ad2597172392935646facc
Hemalatha R <hemalatha@opsera.io>
2023-06-02 22:21:17 -0400

    Commit 1

--- src/main/java/com/opsera/controller/HelloWorldController.java
+++ src/main/java/com/opsera/controller/HelloWorldController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+
     @GetMapping
     public String hello() {
         return "Opsera argo kustomize example....";
